a3c5673f43e712f070d2eb31b52ff6215f0e7262
Vincent Abt <abt.vincent@gmail.com>
2018-04-20 10:10:34 +0200

    pins header, meant to be used as a container for the pin settings

---
+++ Arduino/PlatformIO/src/Util/pins.h
@@ -0,0 +1,14 @@
+// ensure this  is only included once
+#ifndef _PINS_H_
+#define _PINS_H_
+
+#define INCURRENTPIN A6
+#define INVOLTPIN A2
+#define OUTCURRENTPIN A1
+#define OUTVOLTPIN A3
+#define DEBUG_LED 13
+#define POWER_SMPS 4
+#define NEIGHBOR_DEBUG 8
+#define DEBUG_ENABLE 7
+
+#endif
