7706335e2fe72f2915b1b9b569e83befd4214156
Vincent Abt <abt.vincent@gmail.com>
2018-04-20 15:04:33 +0200

    added initialize method for pins

--- Arduino/PlatformIO/src/Util/pins.h
+++ Arduino/PlatformIO/src/Util/pins.h
@@ -2,6 +2,8 @@
 #ifndef _PINS_H_
 #define _PINS_H_
 
+#include <Arduino.h>
+
 // I²C devices
 #define I2C_ADDRESS 0x3f // LCD address to 0x27
 #define MPC4725_ADDRESS 0x62
@@ -14,5 +16,26 @@
 #define POWER_SMPS 4
 #define NEIGHBOR_DEBUG 8
 #define DEBUG_ENABLE 7
+
+class IOSetup
+{
+  public:
+    static void initialize()
+    { //Pins Definieren
+        pinMode(INCURRENTPIN, INPUT);
+        pinMode(INVOLTPIN, INPUT);
+        pinMode(OUTVOLTPIN, INPUT);
+        pinMode(OUTCURRENTPIN, INPUT);
+        pinMode(DEBUG_LED, OUTPUT);
+        pinMode(DEBUG_ENABLE, INPUT_PULLUP);
+        pinMode(NEIGHBOR_DEBUG, OUTPUT);
+        pinMode(POWER_SMPS, OUTPUT);
+
+        //Init Set OutPut Pins
+        digitalWrite(NEIGHBOR_DEBUG, LOW);
+        digitalWrite(DEBUG_LED, LOW);
+        digitalWrite(POWER_SMPS, LOW);
+    }
+};
 
 #endif
